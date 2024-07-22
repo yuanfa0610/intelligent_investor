95ff83db37fb3c1462aafd7c55ba356967223864
Fangzhou Yuan <fyuan@pamperedchef.com>
2024-07-22 17:22:50 -0400

    code for collecting balance sheets

--- src/company.py
+++ src/company.py
@@ -4,6 +4,7 @@ class Company:
     self.name = name
     self.ticker = ticker
     self.income_statements = {}
+    self.balance_sheets = {}
   
   def __str__(self):
     return f"Company(name={self.name}, ticker={self.ticker})"
